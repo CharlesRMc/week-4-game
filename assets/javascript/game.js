d859ba1889132d73aa5f62d98827d1d37d7bee62
CharlesRM7792@gmail.com <CharlesRM7792@gmail.com>
2017-07-09 06:29:29 -0700

    just a few more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,6 @@ var computerGuess = Math.floor(Math.random() *101 + 19)
 var score = 0
 var wins = 0
 var losses = 0
-var totalScore = 0
-var updateScore = function () {
-   	document.querySelector("#score").innerHTML = score;
-	}
 var reset = function(){
 	score = 0;
 	crystalOne = Math.floor(Math.random() *12 + 1);
@@ -31,69 +27,80 @@ console.log(crystalFour)
 console.log(computerGuess)
 console.log(score)
 
+$('#gemMaster').html('Gem Master Score: ' + computerGuess);
+
+
 $('#crystalOne').on('click',function() {
 	score = score + crystalOne;
 	$('#score').html('Score: ' + score);
 	console.log(score)
 	if(score === computerGuess) {
-		console.log('YOU WIN!');
+		alert('YOU WIN!');
 		wins++;
 		$('#wins').html('Wins: ' + wins);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}
 	else if(score > computerGuess) {
-		console.log('YOU LOSE!');
+		alert('YOU LOSE!');
 		losses++;
 		$('#losses').html('Losses: ' + losses);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}});
 $('#crystalTwo').on('click',function() {
 	score = score + crystalTwo;
 	$('#score').html('Score: ' + score);
 	console.log(score)
 	if(score === computerGuess) {
-		console.log('YOU WIN!');
+		alert('YOU WIN!');
 		wins++;
 		$('#wins').html('Wins: ' + wins);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}
 	else if(score > computerGuess) {
-		console.log('YOU LOSE!');
+		alert('YOU LOSE!');
 		losses++;
 		$('#losses').html('Losses:' + losses);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}});
 $('#crystalThree').on('click',function() {
 	score = score + crystalThree;
 	$('#score').html('Score: ' + score);
 	console.log(score)
 	if(score === computerGuess) {
-		console.log('YOU WIN!');
+		alert('YOU WIN!');
 		wins++;
-		$('#win').html('Wins:' + wins);
+		$('#wins').html('Wins:' + wins);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}
 	else if(score > computerGuess) {
-		console.log('YOU LOSE!');
+		alert('YOU LOSE!');
 		losses++;
 		$('#losses').html('Losses:' + losses);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}});
 $('#crystalFour').on('click',function() {
 	score = score + crystalFour;
 	$('#score').html('Score: ' + score);
 	console.log(score)
 	if(score === computerGuess) {
-		console.log('YOU WIN!');
+		alert('YOU WIN!');
 		wins++;
 		$('#wins').html('Wins: ' + wins);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}
 	else if(score > computerGuess) {
-		console.log('YOU LOSE!');
+		alert('YOU LOSE!');
 		losses++;
 		$('#losses').html('Losses: ' + losses);
 		reset();
+		$('#gemMaster').html('Gem Master Score: ' + computerGuess);
 	}});
 
 
